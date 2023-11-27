e7ddeaeb57e3c9bd0d35c40f9ddb4f4a254bacfa
shinnyeonki <sygys10293@gmail.com>
2023-11-28 04:34:41 +0900

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# pdfminer server
+
+1. venv python 가상화를 진행하고(생략가능)
+2. pip 로 필요한 패키지를 설치한다
+```python
+pip install -r requirements.txt
+```
+3. server.py를 실행한다(포트는 5000으로 설정되어있음)
+4. 클라이언트를 실행한다
---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, send_file
+from werkzeug.utils import secure_filename
+from pdf2docx import Converter
+import os
+
+app = Flask(__name__)
+
+@app.route('/pdfminer/get_doc', methods=['POST'])
+def get_doc():
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save(filename)
+
+
+    result_filename = filename.replace('.pdf', '.docx')
+    cv = Converter(filename)
+    cv.convert(result_filename)
+    cv.close()
+    return send_file(result_filename, as_attachment=True) #전송부분이 잘되는지 확인해야함!!
+
+# @app.route('/pdfminer/get_txt', methods=['POST'])
+# def run_python_program():
+#     file = request.files['file']
+#     filename = secure_filename(file.filename)
+#     file.save(filename)
+
+#     # 여기에서 filename 파일을 사용하여 Python 프로그램을 실행하고,
+#     # 결과 파일을 생성합니다. 결과 파일의 이름을 result_filename이라고 가정합니다.
+#     # Python 프로그램 실행 코드를 여기에 작성하세요.
+
+#     return send_file(result_filename, as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

edd3b5f0a48de87a7818be05c596646b63f4ab2c
shinnyeonki <sygys10293@gmail.com>
2023-12-03 17:18:49 +0900

    data 파일에 저장

--- server.py
+++ server.py
@@ -1,22 +1,33 @@
+import os
+import subprocess
+import re
 from pdf2docx import Converter
 from flask import Flask, request, send_file
 from flask_cors import CORS
 from pdfminer.high_level import extract_text
+import base64
 
 app = Flask(__name__)
 CORS(app)
+
+# 데이터를 저장할 폴더
+data_folder = 'data'
+
 
 # pdf  -> docx 변환 기능
 @app.route('/pdfminer/get_doc', methods=['POST'])
 def get_doc():
     file = request.files['file']
     filename = file.filename
-    file.save(filename)
+    # 파일을 'data' 폴더에 저장
+    filepath = os.path.join(data_folder, filename)
+    file.save(filepath)
+    result_filename = filepath.replace('.pdf', '.docx')
 
-    result_filename = filename.replace('.pdf', '.docx')
-    cv = Converter(filename)
+    cv = Converter(filepath)
     cv.convert(result_filename)
     cv.close()
+    
     return send_file(result_filename, as_attachment=True)
 
 
@@ -24,23 +35,43 @@ def get_doc():
 def get_txt():
     file = request.files['file']
     filename = file.filename
-    file.save(filename)
+    # 파일을 'data' 폴더에 저장
+    filepath = os.path.join(data_folder, filename)
+    file.save(filepath)
 
     # PDF 파일에서 텍스트 추출
-    text = extract_text(filename)
+    text = extract_text(filepath)
 
     # 추출된 텍스트를 txt 파일로 저장
-    result_filename = filename.replace('.pdf', '.txt')
+    result_filename = filepath.replace('.pdf', '.txt')
     with open(result_filename, 'w') as f:
         f.write(text)
 
     return send_file(result_filename, as_attachment=True)
 
-# @app.route('/pdfminer/get_txt', methods=['POST'])
-# def get_html():
-#     pass
+
+@app.route('/pdfminer/get_html', methods=['POST'])
+def get_html():
+    file = request.files['file']
+    filename = file.filename
+    # 파일을 'data' 폴더에 저장
+    filepath = os.path.join(data_folder, filename)
+    file.save(filepath)
+    result_filename = filepath.replace('.pdf', '.html')
     
+    # pdf2htmlEX 명령의 인수로 파일의 이름을 넣어주면 html 파일로 변환
+    command = "pdf2htmlEX {}".format(filepath)
+    try:
+        subprocess.run(command, check=True, shell=True)
+    except subprocess.CalledProcessError as e:
+        return str(e)
+    
+    return send_file(result_filename, as_attachment=True)
+
 
 if __name__ == '__main__':
+    # 'data' 폴더가 없으면 생성
+    if not os.path.exists(data_folder):
+        os.makedirs(data_folder)
     app.run(host='0.0.0.0', port=5000)
 #    app.run(host='localhost', port=5000)

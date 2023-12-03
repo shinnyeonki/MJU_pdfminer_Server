c7e15e0ae0e368934d1b514db0db35892de27a06
shinnyeonki <sygys10293@gmail.com>
2023-12-03 19:32:10 +0900

    pdf to html 기능 추가

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from flask import Flask, request, send_file
 from flask_cors import CORS
 from pdfminer.high_level import extract_text
 import base64
+import filename
 
 app = Flask(__name__)
 CORS(app)
@@ -13,22 +14,13 @@ CORS(app)
 # 데이터를 저장할 폴더
 data_folder = 'data'
 
+def base64encode(string):
+    return base64.b64encode(string).decode('utf-8')
 
-# pdf  -> docx 변환 기능
-@app.route('/pdfminer/get_doc', methods=['POST'])
-def get_doc():
-    file = request.files['file']
-    filename = file.filename
-    # 파일을 'data' 폴더에 저장
-    filepath = os.path.join(data_folder, filename)
-    file.save(filepath)
-    result_filename = filepath.replace('.pdf', '.docx')
+def base64decode(string):
+    return base64.b64decode(string).decode('utf-8')
 
-    cv = Converter(filepath)
-    cv.convert(result_filename)
-    cv.close()
-    
-    return send_file(result_filename, as_attachment=True)
+
 
 
 @app.route('/pdfminer/get_txt', methods=['POST'])
@@ -49,6 +41,23 @@ def get_txt():
 
     return send_file(result_filename, as_attachment=True)
 
+# pdf  -> docx 변환 기능
+@app.route('/pdfminer/get_doc', methods=['POST'])
+def get_doc():
+    file = request.files['file']
+    filename = file.filename
+    # 파일을 'data' 폴더에 저장
+    filepath = os.path.join(data_folder, filename)
+    file.save(filepath)
+    result_filename = filepath.replace('.pdf', '.docx')
+
+    cv = Converter(filepath)
+    cv.convert(result_filename)
+    cv.close()
+    
+    return send_file(result_filename, as_attachment=True)
+
+
 
 @app.route('/pdfminer/get_html', methods=['POST'])
 def get_html():
@@ -60,7 +69,14 @@ def get_html():
     result_filename = filepath.replace('.pdf', '.html')
     
     # pdf2htmlEX 명령의 인수로 파일의 이름을 넣어주면 html 파일로 변환
-    command = "pdf2htmlEX {}".format(filepath)
+    # 이스케이프 처리할 문자 패턴
+    escape_pattern = r'[^\w\d\s]'
+
+# 이스케이프 처리 함수
+    def escape_match(match):
+        char = match.group()
+        return '\\' + char
+    command = "./pdfToHtml.sh {}".format(re.sub(escape_pattern, escape_match, filepath))
     try:
         subprocess.run(command, check=True, shell=True)
     except subprocess.CalledProcessError as e:
